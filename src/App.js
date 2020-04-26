dea9b5fb6b1e02a17f656711985306ebedbe2edd
brunomiyamotto <bruno.miyamotto@solera.com>
2020-04-25 22:39:21 -0500

    updae

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
@@ -7,11 +7,11 @@ import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import Test from './components/test/Test';
-
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 class App extends Component {
   render () {
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import '../../App.css';
 
 const Header = props => {
   const { branding } = props;
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
-      <div className="container">
-        <a href="/" className="navbar-brand">
+    <nav className='navbar navbar-expand-sm navbar-dark bg-dark mb-3 py-0'>
+      <div className='container'>
+        <a href='/' className='navbar-brand'>
           {branding}
         </a>
         <div>
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                <i className="fas fa-home" /> Home
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-link'>
+                <i className='fas fa-home' /> Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/contact/add" className="nav-link">
-                <i className="fas fa-plus" /> Add
+            <li className='nav-item'>
+              <Link to='/contact/add' className='nav-link'>
+                <i className='fas fa-plus' /> Add
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link">
-                <i className="fas fa-question" /> About
+            <li className='nav-item'>
+              <Link to='/about' className='nav-link'>
+                <i className='fas fa-question' /> About
               </Link>
             </li>
           </ul>
@@ -35,7 +36,7 @@ const Header = props => {
 };
 
 Header.defaultProps = {
-  branding: 'My App'
+  branding: 'Contact Manager'
 };
 
 Header.propTypes = {
